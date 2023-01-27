4fcf86a933443ea45c69a49e9338e9ff0581a782
Ori Granot <oriorigranot@gmail.com>
2023-01-27 02:13:55 +0200

    test(decorator): fix the tests

--- lib/decorators/olog.decorator.ts
+++ lib/decorators/olog.decorator.ts
@@ -1,16 +1,17 @@
 import "reflect-metadata";
 import { OlogOptions } from "../interfaces/olog-options.interface";
 
-const LOG_KEY = Symbol("olog");
+export const OLOG_KEY = Symbol("olog");
 
 export function olog(options: OlogOptions) {
     return function (target: any, propertyKey: string, descriptor?: PropertyDescriptor) {
         if (!descriptor) {
-            throw "descriptor is null, not a function?"
+            console.log("descriptor is null, not a function?");
+            return;
         }
 
         // apply the decorator to a class method
-        Reflect.defineMetadata(LOG_KEY, options, target, propertyKey);
+        Reflect.defineMetadata(OLOG_KEY, options, target, propertyKey);
         const originalMethod = descriptor.value;
         descriptor.value = function (...args: any[]) {
             console.log(`Calling ${propertyKey} with args: ${args}`);
--- test/decorators/olog.decorator.spec.ts
+++ test/decorators/olog.decorator.spec.ts
@@ -1,9 +1,10 @@
+import { OlogOptions } from './../../dist/interfaces/olog-options.interface.d';
 import { LOG_LEVEL } from './../../lib/enums/log-level.enum';
-import { olog } from '../../lib/decorators/index';
+import { olog, OLOG_KEY } from '../../lib/decorators/index';
 
 class TestClass {
     @olog({ level: LOG_LEVEL.ERROR })
-    sum(a: number, b: number) {
+    public test(a: number, b: number) {
         return a + b;
     }
 }
@@ -11,8 +12,7 @@ class TestClass {
 describe('olog decorator', () => {
     it('should log the method call with correct log level', () => {
         const testClass = new TestClass();
-        console.log = jest.fn();
-        testClass.sum(1, 2);
-        const a = Reflect.getMetadata(Symbol("olog"), testClass);
+        const metadata: OlogOptions = Reflect.getMetadata(OLOG_KEY, testClass, "test");
+        expect(metadata.level).toBe(LOG_LEVEL.ERROR);
     });
 });
